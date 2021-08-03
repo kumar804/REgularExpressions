85d84a273ce6a04dfa18639b879b27bd90c50f6b
kumar804 <kumarsinghmanish547@gmail.com>
2021-08-03 08:10:36 +0530

    Added welcome message

---
+++ Regularexp/RegularExpression.java
@@ -0,0 +1,14 @@
+package Regularexp;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegularExpression {
+
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to regular expression");
+        ;
+    }
+}
