e55a00bbe7246e68bafbbb882fd8445b6fa76a05
kumar804 <kumarsinghmanish547@gmail.com>
2021-08-03 09:58:15 +0530

    password have atleeast one numeric value

--- Regularexp/RegularExpression.java
+++ Regularexp/RegularExpression.java
@@ -52,10 +52,11 @@ public class RegularExpression {
     /*
      passwordValidation is a function to check password validation
       password validation part 1 is enter minimum 8 characters
+      password validation part 3 is having minimum 1 numeric value.
      */
     public static boolean passwordValidation(String password){
 
-        String regex = "^[a-z A-z]{8,}$";
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
         Pattern p = Pattern.compile(regex);
 
         if ( password == null ){
